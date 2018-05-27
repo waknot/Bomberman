f85b133811de389a468179e1a681ca53c6e5e5e0
Mathieu Masera <masera_m@etna-alternance.net>
2018-05-28 01:36:22 +0200

    comments added

--- main.c
+++ main.c
@@ -45,13 +45,15 @@ int main ()
   if (!sprites_img){
     SDL_ShowSimpleMessageBox(0, "img init error", SDL_GetError(), window);
   }
-  
+
+  //we create the image as a texture to insert it in renderer and texture later
   sprite_texture = SDL_CreateTextureFromSurface(renderer, sprites_img);
   
  if (!sprite_texture){
     SDL_ShowSimpleMessageBox(0, "texture image init error", SDL_GetError(), window);
   }
 
+ //we create a clean texture that we use to work with and set in the renderer
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
 			      SDL_TEXTUREACCESS_TARGET, 1024, 768);
   if (!texture){
@@ -62,9 +64,13 @@ int main ()
     SDL_ShowSimpleMessageBox(0, "setting texture in renderer error",
 			     SDL_GetError(), window);
   }
-  
+
+  //we define where is the first pixel of the texture's image we want to use
   SDL_Rect wall_src_rect = {71, 175, 16, 16};
   //  SDL_Rect ground_src_rect = {122, 175, 16, 16};
+  // SDL_Rect  ground_showed_rect = {105, 175, 16 , 16);
+
+  //we loop to make the first line of block and the last line
   for (i = 0; i < 15; i++) { 
     SDL_Rect dest_rect = {i * 48, j * 48 , 48, 48};
     if (SDL_RenderCopy(renderer, sprite_texture , &wall_src_rect, &dest_rect) < 0)
@@ -86,7 +92,7 @@ int main ()
   
   SDL_SetRenderTarget(renderer, NULL);
 
-  SDL_RenderPresent(renderer);
+  //SDL_RenderPresent(renderer);
   while(!quit) {
     SDL_WaitEvent(&event);
 
@@ -101,6 +107,7 @@ int main ()
   }
 
   SDL_DestroyTexture(texture);
+  SDL_DestroyTexture(sprite_texture);
   SDL_FreeSurface(sprites_img);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
