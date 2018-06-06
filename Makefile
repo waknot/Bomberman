64e1422b06405ab7693fe4119f438b11d5138252
Mathieu Masera <masera_m@etna-alternance.net>
2018-06-06 17:32:12 +0200

    code structured changed

--- Makefile
+++ Makefile
@@ -2,9 +2,12 @@ CC 	= gcc
 
 NAME 	= BomberMan
 
-CFLAGS 	= -W -Werror -Wextra -Wall -lSDL2 -lSDL2main -lSDL2_image -lSDL2_ttf -lpthread
+CFLAGS 	= -W -Werror -Wextra -Wall -Iincludes/ -lSDL2 -lSDL2main -lSDL2_image -lSDL2_ttf -lpthread
 
-SRC 	= main.c
+SRC 	= main.c			\
+	  base_map/base_map_manager.c   \
+	  base_map/draw_base_map.c	\
+	  base_map/init_tools.c		\
 
 OBJ 	= ${SRC:%.c=%.o}
 
---
+++ includes/base_map.h
@@ -0,0 +1,47 @@
+#ifndef _BASE_MAP_H_
+#define _BASE_MAP_H_
+
+typedef enum texture_type
+  {
+    wall,
+    ground,
+    ground_shadowed,
+    firstPlayer,
+    pannel,
+    timer
+  } texture_type;
+
+typedef struct s_map
+{
+  SDL_Rect	src_rect;
+  SDL_Rect	dest_rect;
+  texture_type  texture_type;
+} t_map;
+
+typedef struct s_data
+{
+  SDL_Texture	*texture;
+  SDL_Renderer	*renderer;
+  SDL_Window	*window;
+  t_map		array_map[14][15];	
+} t_data;
+
+// in the picture, the size of the square is 16 for a wall block, so i use 48
+
+static const int WINDOW_W = 1024;
+static const int WINDOW_H = 768;
+static const int PIXEL_SIZE = 48;
+static const int I_BEGIN = ((1024 - (15 * 48)) / 2 ) / 48;
+static const int J_BEGIN = (768 - (13 * 48)) / 48;
+
+void *init_window(void * arg);
+void *draw_player_1(void *arg);
+void *draw_map_model(void *arg);
+void *draw_pannel(void *arg);
+void *draw_timer(void *arg);
+void *draw_all(void *arg);
+void *rebuild_map(void *arg);
+SDL_Rect init_rect(int x, int y, int w, int z);
+t_map init_t_map(SDL_Rect src, SDL_Rect dest, texture_type type);
+
+#endif
